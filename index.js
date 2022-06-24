ce5c46dad458faa13e789ed38593edadcc200551
Keshav <keshavarora0114@gmail.com>
2022-06-24 16:00:31 +0530

    make it better

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log(`Backend Server running at ${port}`);
 });
